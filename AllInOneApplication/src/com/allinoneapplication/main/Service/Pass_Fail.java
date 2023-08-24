f70bd45f4e95a9e5b38ee92371c104f261427ae8
Shivam <134369266+ShivamJamwal@users.noreply.github.com>
2023-08-24 13:00:32 +0530

    Update Pass_Fail.java

---
+++ AllInOneApplication/src/com/allinoneapplication/main/Service/Pass_Fail.java
@@ -0,0 +1,27 @@
+package com.allinoneapplication.main;
+
+import java.util.Scanner;
+
+public class Pass_Fail {
+    public Pass_Fail() {
+    }
+
+    public static void pass_fail() {
+        int a;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("PASS-FAIL Calculator");
+            System.out.println("Enter marks:");
+            int marks = scanner.nextInt();
+            if (marks > 33) {
+                System.out.println("" + marks + " is PASS.");
+            } else {
+                System.out.println("" + marks + " is FAIL.");
+            }
+
+            System.out.println("Press 0: For back");
+            a = scanner.nextInt();
+        } while(a != 0);
+
+    }
+}
